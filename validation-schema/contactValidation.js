a03f4afa4389b68b73e84dcb5990734dc3e70b9a
shivamgoswami2711 <shivamgoswami2711gmail.com>
2024-05-15 00:13:09 +0530

    validation

--- validation-schema/contactValidation.js
+++ validation-schema/contactValidation.js
@@ -2,9 +2,7 @@ const Joi = require("joi");
 
 // Define Joi schema for contact fields
 const ValidationObj = {
-  property_id: Joi.string().required().messages({
-    "any.required": "Property ID is required",
-  }),
+
   name: Joi.string().required().messages({
     "any.required": "Name is required",
   }),
@@ -15,15 +13,6 @@ const ValidationObj = {
     "any.required": "Contact number is required",
     "string.unique": "Contact value already exists, please try another value.",
   }),
-  created_at: Joi.date().default(() => new Date()),
-  updated_at: Joi.date().default(() => new Date()),
-  status: Joi.string()
-    .valid("contacted", "interested", "follow-up", "not-interested")
-    .default("interested"),
-  is_fake: Joi.boolean().default(false),
-  email_sent: Joi.boolean().default(false),
-  is_active: Joi.boolean().default(true),
-  is_deleted: Joi.boolean().default(false),
 };
 
 exports.addValidation = Joi.object(ValidationObj);
@@ -31,5 +20,4 @@ exports.addValidation = Joi.object(ValidationObj);
 exports.updateValidation = Joi.object({
   ...ValidationObj,
   id: Joi.string().required(),
-  property_id: Joi.string().optional(),
 });
