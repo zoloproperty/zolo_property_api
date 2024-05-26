b00fe2927b5c409642e3dd6faddb87dc5969da3d
shivamgoswami2711 <shivamgoswami2711gmail.com>
2024-05-26 13:43:19 +0530

    change old db

--- config/database/connection.js
+++ config/database/connection.js
@@ -2,16 +2,16 @@ const mongoose = require("mongoose");
 
 const { DB_USERNAME, DB_PASSWORD } = process.env;
 
-const URL = `mongodb://${DB_USERNAME}:${DB_PASSWORD}@localhost:27017/gproperty`;
-// const URL = `mongodb+srv://${DB_USERNAME}:${DB_PASSWORD}@cluster0.z4sn1bj.mongodb.net/`;
+// const URL = `mongodb://${DB_USERNAME}:${DB_PASSWORD}@localhost:27017/gproperty`;
+const URL = `mongodb+srv://${DB_USERNAME}:${DB_PASSWORD}@cluster0.z4sn1bj.mongodb.net/`;
 
 console.log(URL);
 
 const connectDB = async () => {
   try {
     await mongoose.connect(URL, {
-      // useNewUrlParser: true,
-      // useUnifiedTopology: true,
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
     });
     console.log(`Connected\nDB: ${DB_USERNAME}`);
   } catch (err) {
