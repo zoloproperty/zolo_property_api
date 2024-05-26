0124bda65e2246f38dea4b60d3604489454360f5
shivamgoswami2711 <shivamgoswami2711gmail.com>
2024-05-26 12:35:02 +0530

    update bd str

--- config/database/connection.js
+++ config/database/connection.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const { DB_USERNAME, DB_PASSWORD } = process.env;
 
-const URL = `mongodb://${DB_USERNAME}:${DB_PASSWORD}@localhost:27017/zoloproperty`;
+const URL = `mongodb://${DB_USERNAME}:${DB_PASSWORD}@localhost:27017/gproperty`;
 // const URL = `mongodb+srv://${DB_USERNAME}:${DB_PASSWORD}@cluster0.z4sn1bj.mongodb.net/`;
 
 console.log(URL);
