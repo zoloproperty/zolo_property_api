55f57510dc183e9b9e0d5b8c8e3bb876980243b1
dreamers <ankit@codesmith.app>
2025-08-28 10:52:19 +0530

    incorrect logic fixed

--- controllers/mobileapp/propertyController.js
+++ controllers/mobileapp/propertyController.js
@@ -62,9 +62,7 @@ exports.getMyPropertyIds = async (req, res) => {
 
 exports.getAppVersion =  async (req, res) => {
   try {
-    return new Response(200, "T", {version: process.env.APP_VERSION}).custom(
-      "App version get successfully"
-    );
+    return res.status(200).json({version: process.env.APP_VERSION});
   } catch (error) {
     return res.json(new Response(500, "F").custom(error.message));
   }
